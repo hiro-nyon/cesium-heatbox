2d78e754bbb89fa8c7f32290dd5292974432692c
hiro-nyon <hironobu.k@keio.jp>
2025-09-14 08:32:28 +0900

    chore(release): 0.1.12-alpha.10

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ export { Heatbox as CesiumHeatbox };
  * Library metadata.
  * ライブラリのメタ情報。
  */
-export const VERSION = '0.1.12-alpha.9';
+export const VERSION = '0.1.12-alpha.10';
 export const AUTHOR = 'hiro-nyon';
 export const REPOSITORY = 'https://github.com/hiro-nyon/cesium-heatbox';
 
